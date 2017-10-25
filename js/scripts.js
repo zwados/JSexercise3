fb640a8c1959cb8aa7d36ad633e76839f5b4f87b
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-10-25 19:36:17 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// script.js exercise / comments | debugging| embedding
+var welcome = alert('Welcome to my website, fasten your seatbelts and get ready to see the rocket science!');
+console.log(welcome);
+/*
+This is a block comment
+*/
+// var goodbye = alert('It is not the right time to say goodbye')
